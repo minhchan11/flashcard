921850abf57d5328453d11240ba6d62539e0ec37
Minh Phuong and sandro mijatovic <minh@>
2017-01-10 14:04:08 -0800

    Create effect with JS

---
+++ JS/scripts.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+  $("#def1").click(function() {
+    $("#def1 .keyword").toggle();
+    $("#def1 .definition").toggle();
+  });
+
+  $("#def2").click(function() {
+    $("#def2 .keyword").slideToggle();
+    $("#def2 .definition").slideToggle();
+  });
+
+  $("#def3").click(function() {
+    $("#def3 .keyword").toggle();
+    $("#def3 .definition").toggle();
+  });
+
+  $("#def4").click(function() {
+    $("#def4 .keyword").slideToggle();
+    $("#def4 .definition").slideToggle();
+  });
+
+  $("#def5").click(function() {
+    $("#def5 .keyword").toggle();
+    $("#def5 .definition").toggle();
+  });
+
+  $("#def6").click(function() {
+    $("#def6 .keyword").slideToggle();
+    $("#def6 .definition").slideToggle();
+  });
+
+  $("#def7").click(function() {
+    $("#def7 .keyword").toggle();
+    $("#def7 .definition").toggle();
+  });
+
+  $("#def8").click(function() {
+    $("#def8 .keyword").slideToggle();
+    $("#def8 .definition").slideToggle();
+  });
+
+  $("#def9").click(function() {
+    $("#def9 .keyword").toggle();
+    $("#def9 .definition").toggle();
+  });
+
+  $("#def10").click(function() {
+    $("#def10 .keyword").slideToggle();
+    $("#def10 .definition").slideToggle();
+  });
+
+  $("#def11").click(function() {
+    $("#def11 .keyword").toggle();
+    $("#def11 .definition").toggle();
+  });
+
+  $("#def12").click(function() {
+    $("#def12 .keyword").slideToggle();
+    $("#def12 .definition").slideToggle();
+  });
+
+  $("#def13").click(function() {
+    $("#def13 .keyword").toggle();
+    $("#def13 .definition").toggle();
+  });
+
+  $("#def14").click(function() {
+    $("#def14 .keyword").slideToggle();
+    $("#def14 .definition").slideToggle();
+  });
+
+  $("#def15").click(function() {
+    $("#def15 .keyword").toggle();
+    $("#def15 .definition").toggle();
+  });
+
+  $("#def16").click(function() {
+    $("#def16 .keyword").slideToggle();
+    $("#def16 .definition").slideToggle();
+  });
+
+  $("#def17").click(function() {
+    $("#def17 .keyword").slideToggle();
+    $("#def17 .definition").slideToggle();
+  });
+
+  $("#def18").click(function() {
+    $("#def18 .keyword").toggle();
+    $("#def18 .definition").toggle();
+  });
+
+  $("#def19").click(function() {
+    $("#def19 .keyword").slideToggle();
+    $("#def19 .definition").slideToggle();
+  });
+
+});
